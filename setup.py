e8ba96c99aaf03562509db2d483c4a43d19076cf
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-03-16 20:04:14 +0100

    Update template

--- setup.py
+++ setup.py
@@ -4,18 +4,11 @@
 # nnutil - Neural network utilities for tensorflow
 # Copyright (c) 2018, Abdó Roig-Maranges <abdo.roig@gmail.com>
 #
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
+# This file is part of 'nnutil'.
 #
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This file may be modified and distributed under the terms of the 3-clause BSD
+# license. See the LICENSE file for details.
+
 
 from setuptools import setup, find_packages
 from nnutil import __version__
@@ -23,7 +16,7 @@ from nnutil import __version__
 setup(
     name             = 'nnutil',
     version          = __version__,
-    license          = 'GNU GPLv3',
+    license          = 'BSD',
     description      = 'Neural network utilities for tensorflow',
     author           = 'Abdó Roig-Maranges',
     author_email     = 'abdo.roig@gmail.com',
--- tests/__init__.py
+++ tests/__init__.py
@@ -4,17 +4,10 @@
 # nnutil - Neural network utilities for tensorflow
 # Copyright (c) 2018, Abdó Roig-Maranges <abdo.roig@gmail.com>
 #
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
+# This file is part of 'nnutil'.
 #
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This file may be modified and distributed under the terms of the 3-clause BSD
+# license. See the LICENSE file for details.
+
 
 from tests.test import *
