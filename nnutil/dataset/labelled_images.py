85df5c3f8f2dd074bcc7ce030ac256499ea81814
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-03-25 13:06:09 +0200

    guess labels from directories if not given

--- nnutil/dataset/labelled_images.py
+++ nnutil/dataset/labelled_images.py
@@ -2,8 +2,11 @@ import os
 from .labelled import labelled
 from .image_files import image_files
 
-def labelled_images(path, shape, labels):
+def labelled_images(path, shape, labels=None):
     path = os.path.abspath(path)
+
+    if labels is None:
+        labels = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
 
     dataset = labelled({
         lb: image_files(os.path.join(path, lb),
