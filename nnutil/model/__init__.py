c4e00cda6a1822589f8ca2dd784bcc94086fe16b
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-04-03 21:37:23 +0200

    remove nlmodel stuff

--- nnutil/model/__init__.py
+++ nnutil/model/__init__.py
@@ -2,5 +2,3 @@ from .base_model import *
 from .autoencoder_model import *
 from .classification_model import *
 from .wgan_model import *
-from .nlmodel_writer import *
-from .nlconvnet_model import *
