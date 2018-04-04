dbfd4df2fe0620935876b91cd23642e2fde298f7
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-04-04 23:16:24 +0200

    add confusion mosaic

--- nnutil/util/mosaic.py
+++ nnutil/util/mosaic.py
@@ -26,3 +26,17 @@ def mosaic(image):
     shape = tf.shape(mosaic)
     mosaic = tf.reshape(mosaic, shape=(1, shape[0] * shape[1], shape[2] * shape[3], shape[4]))
     return mosaic
+
+
+def confusion_mosaic(image, nlabels, labels, predictions):
+    indexes = tf.stack([
+        tf.cast(labels, dtype=tf.int32),
+        tf.cast(predictions, dtype=tf.int32)
+    ], axis=-1)
+    mosaic = tf.scatter_nd(indexes, image, shape=(nlabels, nlabels) + tuple(image.shape)[1:])
+
+    # reshape it
+    mosaic = tf.transpose(mosaic, perm=[0, 2, 1, 3, 4])
+    shape = tf.shape(mosaic)
+    mosaic = tf.reshape(mosaic, shape=(1, shape[0] * shape[1], shape[2] * shape[3], shape[4]))
+    return mosaic
