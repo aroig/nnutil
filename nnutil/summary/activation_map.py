2a77ee467532f790c663efffcb3f431029e8266f
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-04-12 18:45:03 +0200

    summary: make images are normalized appropriately

--- nnutil/summary/activation_map.py
+++ nnutil/summary/activation_map.py
@@ -10,6 +10,15 @@ def activation_map(name, logits, image, family=None):
 
         heatmap = tf.abs(heatmap)
         heatmap = heatmap / tf.reduce_max(heatmap)
+        heatmap = tf.image.convert_image_dtype(heatmap, dtype=tf.uint8, saturate=True)
+
+        image = tf.image.convert_image_dtype(image, dtype=tf.uint8, saturate=True)
+
+        pair_image = tf.concat([tf.expand_dims(image, 0),
+                                tf.expand_dims(heatmap, 0)], axis=2)
+
+        summary = tf.summary.image('map', pair_image, family=family)
+
 
         masked_image = tf.concat([tf.expand_dims(image, 0),
                                   tf.expand_dims(heatmap, 0)], axis=2)
