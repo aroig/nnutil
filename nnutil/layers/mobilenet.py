3b153a5f9478f580c19d5d397a35cc9d4d628f74
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-04-12 18:37:57 +0200

    finish mobilenet layer

--- nnutil/layers/mobilenet.py
+++ nnutil/layers/mobilenet.py
@@ -1,15 +1,65 @@
 import os
 import tensorflow as tf
 
+from tensorflow.python.framework.tensor_spec import TensorSpec
+
 class Mobilenet(tf.layers.Layer):
-    def __init__(self, **kwargs):
+    def __init__(self, layer=None, **kwargs):
         super(Mobilenet, self).__init__(**kwargs)
         self._path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                   'mobilenet_v1_1.0_224_frozen.pb')
+        self._layer_spec = [
+            TensorSpec((112, 112, 64), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_1_pointwise/Relu6"),
+
+            TensorSpec((56, 56, 128), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_2_pointwise/Relu6"),
+
+            TensorSpec((56, 56, 128), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_3_pointwise/Relu6"),
+
+            TensorSpec((28, 28, 256), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_4_pointwise/Relu6"),
+
+            TensorSpec((28, 28, 256), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_5_pointwise/Relu6"),
+
+            TensorSpec((14, 14, 512), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_6_pointwise/Relu6"),
+
+            TensorSpec((14, 14, 512), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_7_pointwise/Relu6"),
+
+            TensorSpec((14, 14, 512), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_8_pointwise/Relu6"),
+
+            TensorSpec((14, 14, 512), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_9_pointwise/Relu6"),
+
+            TensorSpec((14, 14, 512), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_10_pointwise/Relu6"),
+
+            TensorSpec((14, 14, 512), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_11_pointwise/Relu6"),
+
+            TensorSpec((7, 7, 1024), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_12_pointwise/Relu6"),
+
+            TensorSpec((7, 7, 1024), tf.float32,
+                       name="MobilenetV1/MobilenetV1/Conv2d_13_pointwise/Relu6")
+        ]
+
+        if layer is None:
+            layer = -1
+
+        if layer < 0:
+            layer = len(self._layer_spec) + layer
+
+        self._output_layer = layer
 
         self._graph_def = None
         self._input_shape = (224, 224, 3)
-        self._output_shape = (1001,)
+        self._output_shape = tuple(self._layer_spec[self._output_layer].shape)
 
     def build(self, input_shape):
         with open(self._path, 'rb') as fd:
@@ -25,16 +75,18 @@ class Mobilenet(tf.layers.Layer):
         results = tf.import_graph_def(
             self._graph_def,
             input_map={'input': inputs},
-            return_elements=["MobilenetV1/Predictions/Softmax"],
+            return_elements=[ls.name for ls in self._layer_spec],
             name=self.name
         )
 
-        output = results[0].outputs[0]
+        self._outputs = [op.outputs[0] for op in results]
 
-        if tuple(output.shape[1:]) != self._output_shape:
-            raise Exception("Output shape does not match")
+        for i, (x, ls) in enumerate(zip(self._outputs, self._layer_spec)):
+            if x.shape[1:] != ls.shape:
+                raise Exception("Output shape does not match layer:\n layer: {}\n name: {}\n spec: {}\n tensor: {}".format(
+                    i, ls.name, ls.shape.as_list(), x.shape.as_list()[1:]))
 
-        return output
+        return self._outputs[self._output_layer]
 
     def compute_output_shape(self, input_shape):
-        return (input_shape[0],) + self._output_shape
+        return (input_shape[0],) + tuple(self._layer_spec[self._output_layer].shape)
