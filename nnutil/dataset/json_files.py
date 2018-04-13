7dd324c98418497100c245a2313bb108cd4524e7
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-04-13 16:06:58 +0200

    dataset: add path_key to json_files

--- nnutil/dataset/json_files.py
+++ nnutil/dataset/json_files.py
@@ -6,18 +6,25 @@ import numpy as np
 from tensorflow.python.data.util import nest
 
 from .parse_json import parse_json
+from .merge import merge
 
 class JSONFiles(tf.data.Dataset):
     def __init__(self, directory, input_spec,
-                 glob='*.json', flatten_lists=False, shuffle=True):
+                 glob='*.json', flatten_lists=False, path_key=None, shuffle=True):
         self._directory = directory
         self._glob = glob
 
-        dataset = tf.data.Dataset.list_files(os.path.join(self._directory, self._glob),
-                                             shuffle=shuffle)
+        files_dataset = tf.data.Dataset.list_files(
+            os.path.join(self._directory, self._glob),
+            shuffle=shuffle)
 
-        dataset = dataset.map(self.load_content)
+        dataset = files_dataset.map(self.load_content)
         dataset = parse_json(dataset, input_spec, flatten_lists=flatten_lists)
+
+        self._path_key = path_key
+
+        if self._path_key is not None:
+            dataset = tf.data.Dataset.zip((dataset, files_dataset)).map(self.attach_path)
 
         self._dataset = dataset
 
@@ -40,6 +47,10 @@ class JSONFiles(tf.data.Dataset):
         content = tf.read_file(path)
         return content
 
+    def attach_path(self, feature, path):
+        feature[self._path_key] = path
+        return feature
+
 def json_files(directory, input_spec,
-               flatten_lists=False, glob='*.json', shuffle=True):
-    return JSONFiles(directory, input_spec, flatten_lists=flatten_lists, glob=glob, shuffle=shuffle)
+               flatten_lists=False, path_key=None, glob='*.json', shuffle=True):
+    return JSONFiles(directory, input_spec, flatten_lists=flatten_lists, path_key=path_key, glob=glob, shuffle=shuffle)
