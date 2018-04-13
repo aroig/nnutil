d01cbab455573970dac9d52ed1d1f6cbeba7b11d
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-04-13 16:08:50 +0200

    check if crop-window is valid before cropping

--- nnutil/dataset/attach_image.py
+++ nnutil/dataset/attach_image.py
@@ -11,7 +11,7 @@ class AttachImage(tf.data.Dataset):
         self._image_key = image_key
 
         if image_path is None:
-            self._image_path_fn = lambda feature: feature["path"]
+            self._image_path_fn = lambda feature: feature["image"]
 
         elif type(image_path) == str:
             self._image_path_fn = lambda feature: feature[image_path]
@@ -23,7 +23,7 @@ class AttachImage(tf.data.Dataset):
             raise Exception("Cannot handle image_path")
 
         if crop_window is None:
-            self._crop_window_fn = lambda feature: None
+            self._crop_window_fn = None
 
         elif type(crop_window) == str:
             self._crop_window_fn = lambda feature: feature[crop_window]
@@ -34,7 +34,13 @@ class AttachImage(tf.data.Dataset):
         else:
             raise Exception("Cannot handle crop_window")
 
-        self._dataset = dataset.map(self.read_image)
+        dataset = dataset.map(self.read_image)
+
+        if self._crop_window_fn is not None:
+            dataset = dataset.filter(self.is_crop_window_contained)
+            dataset = dataset.map(self.crop_image)
+
+        self._dataset = dataset
 
     @property
     def output_classes(self):
@@ -51,6 +57,19 @@ class AttachImage(tf.data.Dataset):
     def _as_variant_tensor(self):
         return self._dataset._as_variant_tensor()
 
+    def is_crop_window_contained(self, feature):
+        crop_window = tf.cast(self._crop_window_fn(feature), dtype=tf.float32)
+        image_shape = tf.cast(tf.shape(feature[self._image_key]), dtype=tf.float32)
+
+        zero = tf.constant(0, dtype=tf.float32)
+
+        C0 = tf.less_equal(zero, crop_window[1])
+        C1 = tf.less_equal(zero, crop_window[0])
+        C2 = tf.less(crop_window[0] + crop_window[2], image_shape[0])
+        C3 = tf.less(crop_window[1] + crop_window[3], image_shape[1])
+
+        return tf.logical_and(tf.logical_and(C0, C1), tf.logical_and(C2, C3))
+
     def read_image(self, feature):
         path = self._image_path_fn(feature)
         data = tf.read_file(path)
@@ -59,16 +78,18 @@ class AttachImage(tf.data.Dataset):
         image = tf.image.decode_image(data, channels=nchannels)
         image = tf.image.convert_image_dtype(image, dtype=tf.float32)
 
-        # TODO: fused decode_and_crop for jpeg
-        # image = tf.image.decode_and_crop_jpeg(data, crop_window, channels=nchannels)
+        feature[self._image_key] = image
+        return feature
 
-        crop_window = self._crop_window_fn(feature)
-        if crop_window is not None:
-            image = tf.image.crop_to_bounding_box(image,
-                                                  crop_window[0],
-                                                  crop_window[1],
-                                                  crop_window[2],
-                                                  crop_window[3])
+    def crop_image(self, feature):
+        crop_window = tf.cast(self._crop_window_fn(feature), dtype=tf.int32)
+        image = feature[self._image_key]
+
+        image = tf.image.crop_to_bounding_box(image,
+                                              crop_window[0],
+                                              crop_window[1],
+                                              crop_window[2],
+                                              crop_window[3])
 
         if self._shape is not None:
             # resize_image wants a static shape, but the kernel does
@@ -80,7 +101,6 @@ class AttachImage(tf.data.Dataset):
         feature[self._image_key] = image
 
         return feature
-
 
 
 def attach_image(dataset, shape, image_key=None, image_path=None, crop_window=None):
