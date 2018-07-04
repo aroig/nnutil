6a82c7e1094aac64b89b08cfd84761859aa99f84
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-07-04 07:31:12 +0200

    dataset: do image rotations in crop_image

--- nnutil/dataset/crop_image.py
+++ nnutil/dataset/crop_image.py
@@ -1,16 +1,29 @@
 import tensorflow as tf
 import numpy as np
 import multiprocessing
+import math
 
 class CropImage(tf.data.Dataset):
-    def __init__(self, dataset, shape, image_key=None, crop_key=None):
+    def __init__(self, dataset, shape, image_key=None, crop_key=None, angle_key=None):
         self._shape = shape
-        self._parallel = int (0.8 * multiprocessing.cpu_count())
+        self._parallel = int (0.5 * multiprocessing.cpu_count())
 
         if image_key is None:
             image_key = 'image'
 
         self._image_key = image_key
+
+        if angle_key is None:
+            self._angle_fn = None
+
+        elif type(angle_key) == str:
+            self._angle_fn = lambda feature: feature[angle_key]
+
+        elif  hasattr(angle_key, '__call__'):
+            self._angle_fn = angle_key
+
+        else:
+            raise Exception("Cannot handle angle_key value")
 
         if crop_key is None:
             self._crop_window_fn = None
@@ -22,13 +35,10 @@ class CropImage(tf.data.Dataset):
             self._crop_window_fn = crop_key
 
         else:
-            raise Exception("Cannot handle crop_window")
+            raise Exception("Cannot handle crop_key value")
 
-        if self._crop_window_fn is not None:
-            # dataset = dataset.filter(self.is_crop_window_contained)
-            dataset = dataset.map(self.crop_image, num_parallel_calls=self._parallel)
-        else:
-            dataset = dataset.map(self.resize_image, num_parallel_calls=self._parallel)
+        # dataset = dataset.filter(self.is_crop_window_contained)
+        dataset = dataset.map(self.crop_image, num_parallel_calls=self._parallel)
 
         self._dataset = dataset
 
@@ -61,48 +71,50 @@ class CropImage(tf.data.Dataset):
         return tf.logical_and(tf.logical_and(C0, C1), tf.logical_and(C2, C3))
 
     def crop_image(self, feature):
-
-        # (y0, x0, y1, x1) crop window in relative coordinates
-        crop_window = self._crop_window_fn(feature)
-
         image = feature[self._image_key]
+        shape = image.shape
 
         # NOTE: We rotate a standardized image, so that the padding is done with a value that becomes zero
-        mu, variance = tf.nn.moments(image, axes=[0, 1, 2])
-        norm_image = tf.image.per_image_standardization(image)
+        mu, variance = tf.nn.moments(image, axes=[-3, -2, -1])
+        epsilon = 1e-4
+        sigma = tf.sqrt(variance) + epsilon
+        norm_image = (image - mu) / sigma
 
-        norm_image = tf.image.crop_and_resize(
-            tf.expand_dims(norm_image, axis=0),
-            tf.expand_dims(crop_window, axis=0),
-            tf.constant([0], dtype=tf.int32),
-            self._shape[0:2],
-            extrapolation_value=0)
+        if self._angle_fn is not None:
+            angle = (2 * math.pi / 360) * self._angle_fn(feature)
+            norm_image = tf.contrib.image.rotate(norm_image, angle, interpolation='BILINEAR')
 
-        image = tf.clip_by_value(mu + tf.sqrt(variance) * norm_image, 0.0, 1.0)
+        if self._crop_window_fn is not None:
+            # (y0, x0, y1, x1) crop window in relative coordinates
+            crop_window = self._crop_window_fn(feature)
 
-        image = tf.squeeze(image, axis=0)
-        image.set_shape(self._shape)
+            if len(shape) == 3:
+                norm_image = tf.expand_dims(norm_image, axis=0)
+                crop_window = tf.expand_dims(crop_window, axis=0)
+
+            norm_image = tf.image.crop_and_resize(
+                norm_image,
+                crop_window,
+                tf.constant([0], dtype=tf.int32),
+                self._shape[0:2],
+                extrapolation_value=0)
+
+            if len(shape) == 3:
+                norm_image = tf.squeeze(norm_image, axis=0)
+
+        elif self._shape is not None:
+            norm_image = tf.image.resize_images(norm_image, size=self._shape[0:2], method=tf.image.ResizeMethod.BILINEAR)
+
+        image = tf.clip_by_value(mu + (sigma + epsilon) * norm_image, 0.0, 1.0)
         feature[self._image_key] = image
 
         return feature
 
-    def resize_image(self, feature):
-        image = feature[self._image_key]
 
-        if self._shape is not None:
-            # resize_image wants a static shape, but the kernel does
-            # not seem to use it. Let's fake it.
-            image.set_shape([None, None, None])
-            image = tf.image.resize_images(image, size=self._shape[0:2], method=tf.image.ResizeMethod.BILINEAR)
-            image.set_shape(self._shape)
-
-        feature[self._image_key] = image
-        return feature
-
-
-def crop_image(dataset, shape, image_key=None, crop_key=None):
+def crop_image(dataset, shape, image_key=None, crop_key=None, angle_key=None):
     return CropImage(
         dataset,
         shape,
         image_key=image_key,
-        crop_key=crop_key)
+        crop_key=crop_key,
+        angle_key=angle_key)
