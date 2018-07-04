7663aea389191ac10a1c4426b230c06270fa8592
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-07-04 07:24:20 +0200

    math: improve approximate identity in the 2D case

--- nnutil/math/approximate_identity.py
+++ nnutil/math/approximate_identity.py
@@ -24,14 +24,23 @@ def closest_fraction(x, N):
 
     return (best_num, best_den)
 
-def approximate_identity(x, shape):
-    """Produces a tensor of given shape from x, which is close to the identity.
-       In particular, if x.shape = shape, it is exactly the identity."""
-    input_shape = tuple(x.shape.as_list()[1:])
-    output_shape = tuple(shape)[1:]
+def _approximate_identity_2d(x, input_shape, output_shape):
+    assert(len(input_shape) == 3)
+    assert(len(output_shape) == 3)
 
-    assert(len(input_shape) == len(output_shape))
+    if (input_shape[0] != output_shape[0] or input_shape[1] != output_shape[1]):
+        x = tf.image.resize_bilinear(x, (output_shape[0], output_shape[1]))
 
+    if input_shape[-1] < output_shape[-1]:
+        padding = tf.constant([[0, 0], [0, 0], [0, 0], [0, output_shape[-1] - input_shape[-1]]])
+        x = tf.pad(x, padding)
+
+    elif input_shape[-1] > output_shape[-1]:
+        raise NotImplementedError
+
+    return x
+
+def _approximate_identity_nd(x, input_shape, output_shape):
     expansion_shape = []
     compression_shape = []
 
@@ -63,6 +72,21 @@ def approximate_identity(x, shape):
 
     xshape = tuple(x.shape.as_list()[1:])
     assert(xshape == output_shape)
+
+    return x
+
+def approximate_identity(x, shape):
+    """Produces a tensor of given shape from x, which is close to the identity.
+       In particular, if x.shape = shape, it is exactly the identity."""
+    input_shape = tuple(x.shape.as_list()[1:])
+    output_shape = tuple(shape)[1:]
+
+    assert(len(input_shape) == len(output_shape))
+
+    if (len(input_shape) == 3):
+        x = _approximate_identity_2d(x, input_shape, output_shape)
+    else:
+        x = _approximate_identity_nd(x, input_shape, output_shape)
 
     return x
 
